ac975d6546ac27d019828aa2c633817da6324967
yashagrawal5757 <yashagrawal5757@gmail.com>
2020-06-10 10:54:28 +0530

    Final version

---
+++ script.js
@@ -0,0 +1,284 @@
+$(function()
+    {
+        $('.map').maphilight();
+    }
+);
+var HTML = "<table border=1 width=30%><tr>";
+
+HTML += "<td align=center>"+"Exam Department"+"</td></tr><br/>";
+HTML += "<tr><td align=center>"+"Heritage Bulding"+"</td></tr><br/>";
+
+HTML += "<tr><td align=center>"+"Hall Of Admissions"+"</td></tr><br/>";
+
+
+HTML += "</tr></table>";
+
+
+
+
+var football = "<table border=1 width=30%><tr>";
+football += "<td align=center >"+"<a href='https://www.dsce.edu.in/campus-life/athletics-fitness' >Football Ground</a>"+"</td></tr>" ;
+football += "</tr></table>";
+
+var basketball = "<table border=1 width=30%><tr>";
+basketball += "<td align=center >"+"<a href='https://www.dsce.edu.in/campus-life/athletics-fitness' >Basketball Ground</a>"+"</td></tr>" ;
+basketball += "</tr></table>";
+
+var tennis = "<table border=1 width=30%><tr>";
+tennis += "<td align=center >"+"<a href='https://www.dsce.edu.in/campus-life/athletics-fitness' >Tennis Court</a>"+"</td></tr>" ;
+tennis += "</tr></table>";
+
+
+var lotus = "<table border=1 width=30%><tr>";
+lotus += "<td align=center>"+"Lotus Canteen"+"</td>";
+lotus += "</tr></table>";
+
+var park = "<table border=1 width=30%><tr>";
+park += "<td align=center >"+"Parking</a>"+"</td></tr>" ;
+park += "</tr></table>";
+
+var audi = "<table border=1 width=30%><tr>";
+audi += "<td align=center >"+"<a href='https://www.dsce.edu.in/campus-life/art-culture' >Auditorium</a>"+"</td></tr>" ;
+audi += "</tr></table>";
+
+
+var cse = "<table border=1 width=30% ><tr>";
+cse += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/computer-science-engineering' >CSE Department</a>"+"</td></tr>" ;
+cse += "<tr><td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/ise' >ISE Department</a>"+"</td></tr>" ;
+cse += "<tr><td align=center>"+"<a href='https://www.dsce.edu.in/academics/common-departments/Physics Department' >Physics Department</a>"+"</td></tr><br/>";
+
+var eee = "<table border=1 width=30% ><tr>";
+eee += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/eee' >EEE Department</a>"+"</td></tr>" ;
+
+var auto = "<table border=1 width=30% ><tr>";
+auto += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/automobile' >Automobile Department</a>"+"</td></tr>" ;
+
+var aero = "<table border=1 width=30% ><tr>";
+aero += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/aeronautical' >Aeronautical Department</a>"+"</td></tr>" ;
+aero += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/common-departments/mathematics' >Mathematics Department</a>"+"</td></tr>" ;
+
+var mech = "<table border=1 width=30% ><tr>";
+mech += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/mechanical' >Mechanical Department</a>"+"</td></tr>" ;
+
+var civil = "<table border=1 width=30% ><tr>";
+civil += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/civil' >Civil Department</a>"+"</td></tr>" ;
+
+var chem = "<table border=1 width=30% ><tr>";
+chem += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/chemical-engg' >Chemical Department</a>"+"</td></tr>" ;
+
+var iem = "<table border=1 width=30% ><tr>";
+iem += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/iem' >IEM Department</a>"+"</td></tr>" ;
+
+var ece = "<table border=1 width=30% ><tr>";
+ece += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/ece' >ECE Department</a>"+"</td></tr>" ;
+ece += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/telecommunication' >Telecom Department</a>"+"</td></tr>" ;
+
+var ei = "<table border=1 width=30% ><tr>";
+ei += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/eie' >IE Department</a>"+"</td></tr>" ;
+
+
+var dental = "<table border=1 width=30% ><tr>";
+dental += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/medical-electronics' >Dental Department</a>"+"</td></tr>" ;
+
+
+var heritage = "<table border=1 width=30% ><tr>";
+heritage += "<td align=center >"+"<a href='https://www.dsce.edu.in/about/administrative-offices' >Heritage Block</a>"+"</td></tr><br/>" ;
+heritage += "<td align=center >"+"<a href='https://www.dsce.edu.in/placements/placements-contact' >Placement Office</a>"+"</td></tr><br/>" ;
+
+var hall = "<table border=1 width=30% ><tr>";
+hall += "<td align=center>"+"<a href='https://www.dsce.edu.in/admissions' >Hall Of Admissions</a>"+"</td></tr><br/>";
+
+var lib = "<table border=1 width=30% ><tr>";
+lib += "<td align=center >"+"<a href='https://www.dsce.edu.in/library' >Main Library</a>"+"</td></tr>" ;
+
+
+
+var cd = "<table border=1 width=30%><tr>";
+cd += "<td align=center >"+"CD Sagar Auditorium </a>"+"</td></tr>" ;
+cd += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/ug/biotechnology' >Biotechnology Department</a>"+"</td></tr>" ;
+cd += "<td align=center >"+"<a href='https://www.dsce.edu.in/placements/cil' >CIL office</a>"+"</td></tr>" ;
+
+cd += "</tr></table>";
+
+var nri = "<table border=1 width=30%><tr>";
+nri += "<td align=center >"+"NRI Boys Hostel </a>"+"</td></tr>" ;
+nri += "<td align=center >"+"<a href='http://dayanandasagar.edu/dsi-360/pano/dsi/canteen/canteen.html' >NRI Food court</a>"+"</td></tr>" ;
+nri += "</tr></table>";
+
+var ihostel = "<table border=1 width=30%><tr>";
+ihostel += "<td align=center >"+"Indian Boys Hostel </a>"+"</td></tr>" ;
+ihostel += "</tr></table>";
+
+var girlhostel = "<table border=1 width=30%><tr>";
+girlhostel += "<td align=center >"+"Girls Hostel </a>"+"</td></tr>" ;
+girlhostel += "</tr></table>";
+
+var pu = "<table border=1 width=30%><tr>";
+pu += "<td align=center >"+"<a href='http://dayanandasagar.edu/dsi-360/pano/dsi/canteen/canteen.html' >PU College</a>"+"</td></tr>" ;
+pu += "</tr></table>";
+
+var bblock = "<table border=1 width=30%><tr>";
+bblock += "<td align=center >"+"Business Block"+"</td></tr>" ;
+bblock += "</tr></table>";
+
+var arch = "<table border=1 width=30%><tr>";
+arch += "<td align=center >"+" Architectural Department"+"</td></tr>" ;
+arch += "<td align=center >"+"<a href='https://www.dsce.edu.in/exam-section' >COE Office</a>"+"</td></tr>" ;
+arch += "</tr></table>";
+
+var amphi = "<table border=1 width=30%><tr>";
+amphi += "<td align=center >"+" Amphitheatre"+"</td></tr>" ;
+amphi += "</tr></table>";
+
+var hospital = "<table border=1 width=30%><tr>";
+hospital += "<td align=center >"+"<a href='https://www.sagarhospitals.in/' >Sagar Hospital</a>"+"</td></tr>" ;
+hospital += "</tr></table>";
+
+var rock = "<table border=1 width=30%><tr>";
+rock += "<td align=center>"+"Rock Garden"+"</td>";
+rock += "</tr></table>";
+
+
+var temple = "<table border=1 width=30%><tr>";
+temple += "<td align=center>"+" Temple"+"</td></tr>";
+temple += "</tr></table>";
+
+var temple2 = "<table border=1 width=30%><tr>";
+temple2 += "<td align=center>"+"Main Temple"+"</td></tr>";
+temple2 += "</tr></table>";
+
+
+var others = "<table border=1 width=30%><tr>";
+others += "<td align=center>"+"Vijaya Bank"+"</td>";
+others += "</tr></table>";
+
+var bschool = "<table border=1 width=30%><tr>";
+bshcool += "<td align=center >"+"<a href='https://www.dsce.edu.in/academics/pg/mba' >Business Scool</a>"+"</td></tr>" ;
+bschool += "</tr></table>";
+
+var icanteen = "<table border=1 width=30%><tr>";
+icanteen += "<td align=center>"+"Canteen"+"</td>";
+icanteen += "</tr></table>";
+
+var sec = "<table border=1 width=30%><tr>";
+sec += "<td align=center>"+"Security Room"+"</td>";
+sec += "</tr></table>";
+
+
+function myheritage() {
+    document.getElementById("demo").innerHTML = heritage;
+}
+
+function myhall() {
+    document.getElementById("demo").innerHTML = hall;
+}
+function myoffice() {
+    document.getElementById("demo").innerHTML = HTML;
+}
+function mylib() {
+    document.getElementById("demo").innerHTML = lib;
+}
+function myfootball() {
+    document.getElementById("demo").innerHTML = football;
+}
+function mybasketball() {
+    document.getElementById("demo").innerHTML = basketball;
+}
+function mytennis() {
+    document.getElementById("demo").innerHTML = tennis;
+}
+function mylotus() {
+    document.getElementById("demo").innerHTML = lotus;
+}
+function mypark() {
+    document.getElementById("demo").innerHTML = park;
+}
+function myaudi()
+{
+    document.getElementById("demo").innerHTML = audi;
+}
+
+function mycse() {
+    document.getElementById("demo").innerHTML = cse;
+}
+function myeee() {
+    document.getElementById("demo").innerHTML = eee;
+}
+function myauto() {
+    document.getElementById("demo").innerHTML = auto;
+}
+function myaero() {
+    document.getElementById("demo").innerHTML = aero;
+}
+function mymech() {
+    document.getElementById("demo").innerHTML = mech;
+}
+function mycivil() {
+    document.getElementById("demo").innerHTML = civil;
+}
+function mychem() {
+    document.getElementById("demo").innerHTML = chem;
+}
+function myiem() {
+    document.getElementById("demo").innerHTML = iem;
+}
+function myece() {
+    document.getElementById("demo").innerHTML = ece;
+}
+function myei() {
+    document.getElementById("demo").innerHTML = ei;
+}
+function mydental() {
+    document.getElementById("demo").innerHTML = dental;
+}
+function mybblock() {
+    document.getElementById("demo").innerHTML = bblock;
+}
+function myarch() {
+    document.getElementById("demo").innerHTML = arch;
+}
+function myicanteen() {
+    document.getElementById("demo").innerHTML = icanteen;
+}
+function myamphi() {
+    document.getElementById("demo").innerHTML = amphi;
+}
+
+
+
+function mycd() {
+    document.getElementById("demo").innerHTML = cd;
+}
+function mynri() {
+    document.getElementById("demo").innerHTML =  nri;
+}
+function myihostel() {
+    document.getElementById("demo").innerHTML =  ihostel;
+}
+function mygirlhostel() {
+    document.getElementById("demo").innerHTML =  girlhostel;
+}
+function mypu() {
+    document.getElementById("demo").innerHTML = pu;
+}function myhospital() {
+    document.getElementById("demo").innerHTML = hospital;
+}
+function myrock() {
+    document.getElementById("demo").innerHTML = rock;
+}
+
+function myothers() {
+    document.getElementById("demo").innerHTML = others;
+}
+function mysec() {
+    document.getElementById("demo").innerHTML = sec;
+}
+function mytemple() {
+    document.getElementById("demo").innerHTML = temple;
+}
+function mytemple2() {
+    document.getElementById("demo").innerHTML = temple2;
+}
+function mybschool() {
+    document.getElementById("demo").innerHTML = bschool;
+}
